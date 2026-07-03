9c24cb2c6db52a1e504b8a5857259e1ce5c5338f
muralivgit <muralikrishna.psl@gmail.com>
2026-07-03 16:35:16 +0530

    formatted

--- src/main/java/org/example/kafkademo/Order.java
+++ src/main/java/org/example/kafkademo/Order.java
@@ -1,24 +1,42 @@
 package org.example.kafkademo;
 
 public class Order {
-    private String orderId;
-    private String itemName;
-    private double price;
+	private String orderId;
+	private String itemName;
+	private double price;
 
-    // Default constructor needed for JSON Deserialization
-    public Order() {}
+	// Default constructor needed for JSON Deserialization
+	public Order() {
+	}
 
-    public Order(String orderId, String itemName, double price) {
-        this.orderId = orderId;
-        this.itemName = itemName;
-        this.price = price;
-    }
+	public Order(String orderId, String itemName, double price) {
+		this.orderId = orderId;
+		this.itemName = itemName;
+		this.price = price;
+	}
 
-    // Getters and Setters
-    public String getOrderId() { return orderId; }
-    public void setOrderId(String orderId) { this.orderId = orderId; }
-    public String getItemName() { return itemName; }
-    public void setItemName(String itemName) { this.itemName = itemName; }
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 }
